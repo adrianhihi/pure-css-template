8eb1050cfa583bf203693d17c4007a3731f68b1a
jeantimex <jean.timex@gmail.com>
2017-01-19 15:41:54 -0800

    Changed 4 spaces to 2.

--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,58 @@
 (function (window, document) {
 
-    var menu = document.getElementById('menu'),
-        WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange' : 'resize';
-    
-    function toggleHorizontal() {
-        [].forEach.call(
-            document.getElementById('menu').querySelectorAll('.nav-can-transform'),
-            function (el) {
-                el.classList.toggle('pure-menu-horizontal');
-            }
-        );
-    };
-    
-    function toggleMenu() {
-        // set timeout so that the panel has a chance to roll up
-        // before the menu switches states
-        if (menu.classList.contains('open')) {
-            setTimeout(toggleHorizontal, 500);
-        }
-        else {
-            toggleHorizontal();
-        }
-        menu.classList.toggle('open');
-        document.getElementById('toggle').classList.toggle('x');
-    };
-    
-    function closeMenu() {
-        if (menu.classList.contains('open')) {
-            toggleMenu();
-        }
+  var menu = document.getElementById('menu'),
+    WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange' : 'resize';
+  
+  function toggleHorizontal() {
+    [].forEach.call(
+      document.getElementById('menu').querySelectorAll('.nav-can-transform'),
+      function (el) {
+        el.classList.toggle('pure-menu-horizontal');
+      }
+    );
+  };
+  
+  function toggleMenu() {
+    // set timeout so that the panel has a chance to roll up
+    // before the menu switches states
+    if (menu.classList.contains('open')) {
+      setTimeout(toggleHorizontal, 500);
     }
-    
-    document.getElementById('toggle').addEventListener('click', function (e) {
-        toggleMenu();
-        e.preventDefault();
-    });
-    
-    window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
+    else {
+      toggleHorizontal();
+    }
+    menu.classList.toggle('open');
+    document.getElementById('toggle').classList.toggle('x');
+  };
+  
+  function closeMenu() {
+    if (menu.classList.contains('open')) {
+      toggleMenu();
+    }
+  }
+  
+  document.getElementById('toggle').addEventListener('click', function (e) {
+    toggleMenu();
+    e.preventDefault();
+  });
+  
+  window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
 
 })(this, this.document);
 
 jQuery(document).ready(function () {
-    // Rs-Slider
-    jQuery('.fullscreenbanner').revolution({
-        delay: 5000,
-        startwidth: 1170,
-        startheight: 745,
-        fullWidth: "on",
-        fullScreen: "off",
-        hideCaptionAtLimit: "",
-        dottedOverlay: "twoxtwo",
-        navigationStyle: "preview4",
-        fullScreenOffsetContainer: "",
-        hideTimerBar:"on"
-    });
+  // Rs-Slider
+  jQuery('.fullscreenbanner').revolution({
+    delay: 5000,
+    startwidth: 1170,
+    startheight: 745,
+    fullWidth: "on",
+    fullScreen: "off",
+    hideCaptionAtLimit: "",
+    dottedOverlay: "twoxtwo",
+    navigationStyle: "preview4",
+    fullScreenOffsetContainer: "",
+    hideTimerBar:"on"
+  });
 });
 
